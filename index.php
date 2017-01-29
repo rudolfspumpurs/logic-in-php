0a7854545dd9ead37e7bc218c3670796b7a09562
Rudolfs Pumpurs <rudolfs.pumpurs@gmail.com>
2017-01-29 17:26:51 +0200

    Add basic query

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+
+include 'vendor/autoload.php';
+
+ORM::configure('mysql:host=localhost;dbname=logic_in_php');
+ORM::configure('username', 'root');
+ORM::configure('password', '');
+
+ORM::for_table('complicated_table')->find_array();
