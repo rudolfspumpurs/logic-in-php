aefb1574ee5bce0fe3ede84ba290e0974284073e
Rudolfs Pumpurs <rudolfs.pumpurs@gmail.com>
2017-01-29 18:04:05 +0200

    Add main test code

--- index.php
+++ index.php
@@ -6,4 +6,36 @@ ORM::configure('mysql:host=localhost;dbname=logic_in_php');
 ORM::configure('username', 'root');
 ORM::configure('password', '');
 
-ORM::for_table('complicated_table')->find_array();
+
+
+$time_start = microtime(true);
+
+$result = ORM::for_table('complicated_table')
+    ->select('name')
+    ->whereRaw('created > NOW() and is_set = 1')
+    ->find_array();
+
+$time_end = microtime(true);
+
+$resultTimeSqlLogic = $time_end - $time_start;
+
+//---------------------------------------------------------------------------
+
+$time_start = microtime(true);
+
+$rows = ORM::for_table('complicated_table')
+    ->selectMany(['name', 'is_set'])
+    ->whereRaw('created > NOW()')
+    ->find_array();
+
+$result2 = [];
+foreach($rows as $row) {
+    if ($row['is_set'] == 1) $result2[] = $row;
+}
+
+$time_end = microtime(true);
+
+$resultTimePhpLogic = $time_end - $time_start;
+
+echo 'Execution time (SQL) : '.$resultTimeSqlLogic.' seconds' . "\n";
+echo 'Execution time (PHP) : '.$resultTimePhpLogic.' seconds' . "\n";
